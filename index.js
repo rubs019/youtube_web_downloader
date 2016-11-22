a3826c04ba233628a1a3144ed4ea5a81bd109830
rubs019 <ruben.desert@emi.com>
2016-11-22 10:47:31 +0100

    changement index

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ var express = require('express');
 var pug = require('pug');
 var app = express();
 var bodyParser = require('body-parser'); // For read json file
-var helmet = require('helmet'); // For security
-var AdmZip = require('adm-zip'); // For Zip File
-var request = require('request'); // For Download mP3
-var fs = require('fs');  
-var download = require('download');
-
-app.use(helmet());
+// var helmet = require('helmet'); // For security
+// var AdmZip = require('adm-zip'); // For Zip File
+// var request = require('request'); // For Download mP3
+// var fs = require('fs');
+// var download = require('download');
+// app.use(helmet());
 
 // 	app.use(bodyParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
 // Set views
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -24,7 +23,7 @@ app.use('/js', express.static(__dirname + '/assets/js'));
 app.use('/img', express.static(__dirname + '/assets/img'));
 
 // GET
-app.get('/', function (req, res) {
+app.get('', function (req, res) {
 	res.render('index', {title: 'Youtube Download'});
 });
 
